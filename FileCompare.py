abcd11107472555a6c1ef8b04329177ce5d3de03
JuMaD <julian.dlugosch@gmail.com>
2019-10-11 14:25:53 +0200

    Added functionality to summarize all averages of odd into one df

--- FileCompare.py
+++ FileCompare.py
@@ -11,6 +11,7 @@ from tkinter import filedialog
 
 import pandas as pd
 from tqdm import tqdm
+import re
 
 
 def join_dfs(ldf,rdf):
@@ -23,6 +24,7 @@ def aggregate_averages():
     dirname = filedialog.askdirectory()
 
     df_dict = {}
+    df_columns = []
     df_odd = []
     df_even = []
     for file in tqdm(os.listdir(dirname)):
@@ -36,15 +38,27 @@ def aggregate_averages():
             #print(file)
             logging.info(f'Opening file: {filename}')
             df = pd.read_csv(filename, sep='\t').set_index('Voltage [V]')
+            regex_size = r'(?<=PS)(.*?)(?=u)'
+            regex_device = r'(?<=u)(.*?)(?=I)'
+            match_size = re.search(regex_size, file)
+            match_device = re.search(regex_device, file)
+            junction_size = match_size.group(0)+'um'
+            device_number = match_device.group(0)[:-1]
+            print(device_number)
 
+
+            df.mean_odd.columns = f'{junction_size} ({device_number})'
+            df_columns.append(df.mean_odd.columns)
             df_odd.append(df.mean_odd)
 
             df_dict[f'{file}'] = df
 
 
     odd_mean = pd.concat(df_odd, axis=1)
-
+    print(df_columns)
+    odd_mean.columns = df_columns
     print(odd_mean.head())
+
 
 
 
